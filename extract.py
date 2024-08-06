10b83f1e48dd10407a214f596b11e79beea6f19b
valpaulo <val.otoman@gmail.com>
2024-08-06 18:28:20 +0800

    new extract.py

---
+++ extract.py
@@ -0,0 +1,40 @@
+import requests
+import pandas as pd
+
+def get_brands():
+    r = requests.get("https://script.google.com/macros/s/AKfycbxNu27V2Y2LuKUIQMK8lX1y0joB6YmG6hUwB1fNeVbgzEh22TcDGrOak03Fk3uBHmz-/exec?route=brand-list")
+    
+    if r.status_code == 200:
+        data = r.json()
+        brands = pd.DataFrame(data.get('data', []))
+
+        return brands
+    else:
+        print(f"Failed to retrieve data. Status code: {r.status_code}")
+
+        return pd.DataFrame()  
+
+def get_devices():
+    r = requests.get("https://script.google.com/macros/s/AKfycbxNu27V2Y2LuKUIQMK8lX1y0joB6YmG6hUwB1fNeVbgzEh22TcDGrOak03Fk3uBHmz-/exec?route=device-list")
+    
+    if r.status_code == 200:
+        data = r.json()
+        all_devices = []
+
+        for brand in data.get("data", []): 
+            device_list = brand.get("device_list", [])
+
+            for device in device_list:
+                device["brand_id"] = brand["brand_id"]
+                device["brand_name"] = brand["brand_name"]
+                device["brand_key"] = brand["key"]
+
+            all_devices.extend(device_list)
+
+        all_devices_df = pd.DataFrame(all_devices)
+
+        return all_devices_df
+    else:
+        print(f"Failed to retrieve data. Status code: {r.status_code}")
+
+        return pd.DataFrame()
--- main.py
+++ main.py
@@ -1,45 +1,6 @@
 import requests
-import json
 import pandas as pd
-from sqlalchemy import create_engine
-
-def get_brands():
-    r = requests.get("https://script.google.com/macros/s/AKfycbxNu27V2Y2LuKUIQMK8lX1y0joB6YmG6hUwB1fNeVbgzEh22TcDGrOak03Fk3uBHmz-/exec?route=brand-list")
-    
-    if r.status_code == 200:
-        data = r.json()
-        brands = pd.DataFrame(data.get('data', []))
-
-        return brands
-    else:
-        print(f"Failed to retrieve data. Status code: {r.status_code}")
-
-        return pd.DataFrame()  
-
-def get_devices():
-    r = requests.get("https://script.google.com/macros/s/AKfycbxNu27V2Y2LuKUIQMK8lX1y0joB6YmG6hUwB1fNeVbgzEh22TcDGrOak03Fk3uBHmz-/exec?route=device-list")
-    
-    if r.status_code == 200:
-        data = r.json()
-        all_devices = []
-
-        for brand in data.get("data", []): 
-            device_list = brand.get("device_list", [])
-
-            for device in device_list:
-                device["brand_id"] = brand["brand_id"]
-                device["brand_name"] = brand["brand_name"]
-                device["brand_key"] = brand["key"]
-
-            all_devices.extend(device_list)
-
-        all_devices_df = pd.DataFrame(all_devices)
-
-        return all_devices_df
-    else:
-        print(f"Failed to retrieve data. Status code: {r.status_code}")
-
-        return pd.DataFrame()
+from extract import get_brands, get_devices
 
 
 def get_device_details(key):
@@ -147,5 +108,3 @@ def load_devices_to_db():
 print(get_brands().head(10))
 print(get_devices().head(10))
 
-
-# get_device_details("apple_iphone_13_pro_max-11089")
